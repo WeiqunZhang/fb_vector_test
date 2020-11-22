c70102ae4b82aafd857b04c3e5be5d964b28198e
Weiqun Zhang <weiqunzhang@lbl.gov>
2020-11-21 17:02:23 -0800

    add check

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[])
             ba.maxSize(mgs);
             ba.convert(IntVect::TheDimensionVector((n+1) % AMREX_SPACEDIM));
             DistributionMapping dm(ba);
-            mf[n].define(ba,dm,1,1);
+            mf[n].define(ba,dm,3,1);
             for (MFIter mfi(mf[n]); mfi.isValid(); ++mfi) {
                 Box const& bx = mfi.validbox();
                 Array4<int> const& fab = mf[n].array(mfi);
@@ -33,6 +33,23 @@ int main(int argc, char* argv[])
         }
 
         FillBoundary(amrex::GetVecOfPtrs(mf));
+
+        for (int n = 0; n < mf.size(); ++n) {
+            for (MFIter mfi(mf[n]); mfi.isValid(); ++mfi) {
+                auto const& fab = mf[n].const_array(mfi);
+                amrex::LoopOnCpu(mfi.fabbox(), [=] (int i, int j, int k)
+                {
+                    if (domain.contains(Dim3{i,j,k})) {
+                        if (fab(i,j,k,0) != i or fab(i,j,k,1) != j or fab(i,j,k,2) != k) {
+                            amrex::AllPrint() << "Failed: " << Dim3{i,j,k} << " "
+                                              << fab(i,j,k,0) << " " << fab(i,j,k,1)
+                                              << " " << fab(i,j,k,2) << std::endl;
+                            amrex::Abort();
+                        }
+                    }
+                });
+            }
+        }
     }
     amrex::Finalize();
 }
